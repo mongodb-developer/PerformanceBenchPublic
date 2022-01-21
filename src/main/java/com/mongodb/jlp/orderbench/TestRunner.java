cc7ef89124f3ef3eb5f10cd9c93f7f00fcb5fe23
John Page <johnlpage@gmail.com>
2022-01-21 16:39:30 +0000

    Threaded V1

--- src/main/java/com/mongodb/jlp/orderbench/TestRunner.java
+++ src/main/java/com/mongodb/jlp/orderbench/TestRunner.java
@@ -1,42 +1,100 @@
 package com.mongodb.jlp.orderbench;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 //This class takes a List of test types and runs them
 
-public class TestRunner {
+public class TestRunner implements Runnable {
 
 	Logger logger;
+	private List<SchemaTest> tests;
+	private TestOptions options;
 	private static Random random = new Random();
+	ExecutorService executorService;
+	SchemaTest test;
+	boolean warmup;
+	int ORDERSTOTEST = 50000;
+	double[] times = new double[ORDERSTOTEST];
+
 	/* Simulates N devices inserting X Documents */
 
 	TestRunner() {
 		logger = LoggerFactory.getLogger(TestRunner.class);
 	}
 
+	void prep(SchemaTest t, TestOptions o, boolean warmup) {
+		options = o;
+		test = t;
+		this.warmup = warmup;
+	}
+
 	void runTest(List<SchemaTest> tests, TestOptions options) {
-		logger.info("Warmup");
+
+		final int NTHREADS = 20; /* Optimal here is actually a key tunable */
+
 		for (SchemaTest s : tests) {
-			getOrdersByIdTest(s, options, true);
-		}
-		logger.info("Test");
-		for (SchemaTest s : tests) {
-			getOrdersByIdTest(s, options, false);
+			executorService = Executors.newFixedThreadPool(NTHREADS);
+			logger.info("Warmup");
+			prep(s, options, true);
+			run();
+			logger.info("Test");
+			List<TestRunner> runners = new ArrayList<TestRunner>();
+
+			for (int t = 0; t < NTHREADS; t++) {
+				TestRunner tr = new TestRunner();
+				tr.prep(s, options, false);
+				runners.add(tr);
+				executorService.execute(tr); // calls run()
+			}
+			long startTime = System.nanoTime();
+			executorService.shutdown();
+			try {
+				executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+			} catch (InterruptedException e) {
+
+			}
+			long endTime = System.nanoTime();
+			double[] alltimes = new double[ORDERSTOTEST * NTHREADS];
+			int i = 0;
+			for (TestRunner t : runners) {
+				System.arraycopy(t.times, 0, alltimes, i, ORDERSTOTEST);
+				i += ORDERSTOTEST;
+			}
+			Arrays.sort(alltimes);
+			int timelen = alltimes.length;
+			long total = 0;
+			for (int t = 0; t < timelen; t++) {
+				total += alltimes[t];
+			}
+
+			double secs = (double) (endTime - startTime) / 1000000000.0;
+
+			logger.info(String.format(
+					"%s max(ms): %.2f min(ms): %.2f mean(ms): %.2f 95th centile(ms): %.2f throughput(qps): %.1f",
+					s.name(),
+					alltimes[timelen - 1] / 1000.0,
+					alltimes[0] / 1000.0,
+					(total / 1000.0) / timelen,
+					alltimes[(int) (timelen * .95)] / 1000.0,
+					(double) timelen / secs));
+			// Gather up the Stats rather then printing and merge them.
 		}
 	}
 
 	void getOrdersByIdTest(SchemaTest s, TestOptions testOptions, boolean warmup) {
-		int ORDERSTOTEST = 50000;
+
 		int custid = random.nextInt(testOptions.getInteger("customers"));
 		int orderid = random.nextInt(testOptions.getInteger("orders"));
 
-		double[] times = new double[ORDERSTOTEST];
-		long total = 0;
 		for (int o = 0; o < ORDERSTOTEST; o++) {
 			String orderId = String.format("C#%dO#%d", custid, orderid);
 			long startTime = System.nanoTime();
@@ -44,14 +102,12 @@ public class TestRunner {
 			long endTime = System.nanoTime();
 			long duration = (endTime - startTime) / 1000; // Microseconds
 			times[o] = duration;
-			total += duration;
-		}
-		if (!warmup) {
-			Arrays.sort(times);
-			logger.info(String.format("%s max(ms): %.2f min(ms): %.2f mean(ms): %.2f 95th centile(ms): %.2f", s.name(),
-					times[ORDERSTOTEST - 1] / 1000.0, times[0] / 1000.0, (total / 1000.0) / ORDERSTOTEST,
-					times[(int) (ORDERSTOTEST * .95)] / 1000.0));
 		}
 	}
 
+	@Override
+	public void run() {
+		// Change this so we can specify what test it should launch.
+		getOrdersByIdTest(test, options, warmup);
+	}
 }
